b402f699d06fd296e444aa67a27ab037df343d32
Aristocrat4 <galadanelia1@gmail.com>
2024-06-30 00:29:30 +0400

    add calendar with custom colors

--- src/app/Pages/fourth-task/fourth-task.component.ts
+++ src/app/Pages/fourth-task/fourth-task.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {
+  MatNativeDateModule,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
+import {
+  MatCalendarCellClassFunction,
+  MatCalendarCellCssClasses,
+  MatDatepickerModule,
+} from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-fourth-task',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatCardModule,
+    MatNativeDateModule,
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './fourth-task.component.html',
-  styleUrl: './fourth-task.component.scss'
+  styleUrl: './fourth-task.component.scss',
 })
 export class FourthTaskComponent {
+  selected = model<Date | null>(null);
 
+  dateClass = (date: Date): MatCalendarCellCssClasses => {
+    const day = date.getDate();
+
+    // Example: Color Sundays red and Saturdays blue
+    if (date.getDay() === 0) return 'sunday-class';
+    if (date.getDay() === 6) return 'saturday-class';
+
+    // Example: Color specific dates
+    if (day === 12) return 'special-date-class';
+
+    return '';
+  };
 }
